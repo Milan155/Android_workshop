0456b0bc3b2e6c67bb7af48841849ff4a5dc6133
Milan155 <141333245+Milan155@users.noreply.github.com>
2023-08-16 15:07:05 +0530

    Add files via upload

---
+++ 16-8-23/assignmnet-7.kt
@@ -0,0 +1,13 @@
+//fun main()
+//{
+//    val str = "this is me"
+//    val ch = "e"
+//    var frequency = 0
+//
+//    for(i in 0..str.length-1){
+//        if(ch == str[1]){
+//            ++frequency
+//        }
+//    }
+//    println("freuency")
+//}
