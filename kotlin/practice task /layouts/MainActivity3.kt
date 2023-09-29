beea9a099a3c1dd77f2440efd7504574098ca28a
Milan155 <141333245+Milan155@users.noreply.github.com>
2023-09-29 16:24:19 +0530

    Add files via upload

---
+++ kotlin/practice task /layouts/MainActivity3.kt
@@ -0,0 +1,32 @@
+package com.example.newlayouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+
+class MainActivity3 : AppCompatActivity() {
+    lateinit var btn1:Button
+    lateinit var btn2:Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taskactivity)
+
+        btn1=findViewById(R.id.btn1)
+        btn2=findViewById(R.id.btn2)
+
+        btn1.setOnClickListener {
+            var i= Intent(applicationContext,MainActivity3::class.java)
+            startActivity(i)
+        }
+        btn2.setOnClickListener {
+            var i= Intent(applicationContext,MainActivity3::class.java)
+            startActivity(i)
+        }
+
+    }
+}
