3fc856ec5ff924d301ec5bfe4c3e325395327433
Milan155 <141333245+Milan155@users.noreply.github.com>
2023-10-05 17:09:32 +0530

    Add files via upload

---
+++ Module4/searchandspinner.kt
@@ -0,0 +1,14 @@
+package com.example.module4part_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class searchandspinner : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchandspinner)
+
+    }
+}
